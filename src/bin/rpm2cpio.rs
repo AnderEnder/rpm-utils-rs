5d8c26f85526653c0041d13223ea6c10a252fc42
Andrii Radyk <ander.ender@gmail.com>
2019-11-05 23:24:46 +0100

    add rpm2cpio as a use case

---
+++ src/bin/rpm2cpio.rs
@@ -0,0 +1,31 @@
+use rpm_utils::RPMFile;
+use std::io;
+use std::path::PathBuf;
+use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rpm-info")]
+struct Args {
+    /// Path to rpm file
+    #[structopt(name = "rpm", parse(from_os_str))]
+    path: PathBuf,
+
+    /// Path to save file
+    #[structopt(long = "output", parse(from_os_str))]
+    output: PathBuf,
+}
+
+fn run(args: Args) -> Result<(), io::Error> {
+    let rpm = RPMFile::open(&args.path)?;
+    rpm.copy_payload(&args.output)?;
+    Ok(())
+}
+
+fn main() {
+    let args = Args::from_args();
+    if let Err(err) = run(args) {
+        eprintln!("{}", err);
+        exit(1);
+    }
+}
